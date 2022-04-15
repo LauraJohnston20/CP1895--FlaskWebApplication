c2bbb1f59dd47b88b72556e46bb89a651c145788
LauraJohnston20 <95102375+LauraJohnston20@users.noreply.github.com>
2022-04-15 18:14:52 -0230

    Add files via upload

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, redirect, session, flash
+import sqlite3
 import csv
 import os
 
 app = Flask(__name__)
 app.config['UPLOAD_PATH'] = 'static/images'
+app.secret_key = "privatekey"
 
 @app.route("/")
 @app.route("/home")
@@ -16,7 +18,7 @@ def home():
 @app.route("/view_recipes")
 def view_recipes():
     recipe_list = read_from_csv()
-    return render_template('view_recipes.html', recipe_list=recipe_list)
+    return render_template('view_recipes.html', recipe_list=recipe_list, title='View Recipes')
 
 
 @app.route("/add_recipes", methods=["POST", "GET"])
@@ -64,14 +66,63 @@ def delete_recipe():
         write_to_csv(recipe_list, columns)
         print(recipe_list)
     else:
-        return render_template('delete_recipe.html', recipe_list=recipe_list)
-    return render_template('delete_recipe.html', recipe_list=recipe_list)
+        return render_template('delete_recipe.html', recipe_list=recipe_list, title='Delete Recipe')
+    return render_template('delete_recipe.html', recipe_list=recipe_list, title='Delete Recipe')
 
 
-@app.route("/login", methods=["POST", "GET"])
+@app.route('/login', methods=['POST', 'GET'])
 def login():
+    if request.method == 'POST':
+        username = request.form["username"]
+        password = request.form["password"]
+        conn = sqlite3.connect("register.db")
+        c = conn.cursor()
+        c.execute("SELECT * FROM user")
+        r = c.fetchall()
+        for i in r:
+            if username == i[0] and password == i[1]:
+                session["logged in"] = True
+                session["username"] = username
+                conn.commit()
+                conn.close()
+                return redirect(url_for("home"))
+        else:
+            flash("Username or password are incorrect. Please try again.")
+
     return render_template('login.html')
 
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if username != "" and password != "":
+            conn = sqlite3.connect("register.db")
+            cursor = conn.cursor()
+            cursor.execute("SELECT * FROM user")
+            r = cursor.fetchall()
+            print(r)
+            for i in r:
+                print(username, i[0])
+                if username == i[0]:
+                    flash("Username taken. Please try again.")
+                    return redirect(url_for("register"))
+
+            sql = 'INSERT INTO user (username, password) VALUES(?,?)'
+            val = (username, password)
+            cursor.execute(sql, val)
+            flash("Your account has been created. Please log in.")
+            conn.commit()
+            conn.close()
+            return redirect(url_for("login"))
+        else:
+            flash("Something went wrong")
+    return render_template('register.html')
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for("home"))
 
 def write_to_csv(all_recipes, columns):
     with open("recipes.csv", 'w', newline='') as csvfile:
